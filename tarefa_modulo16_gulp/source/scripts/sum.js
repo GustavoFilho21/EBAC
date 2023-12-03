1351493b94e819f7348064fbb6f3fa8061133e5f
Gustavo Filho <110490140+GustavoFilho21@users.noreply.github.com>
2023-12-03 12:20:32 -0300

    add tarefa exercicio 16

---
+++ tarefa_modulo16_gulp/source/scripts/sum.js
@@ -0,0 +1,17 @@
+function somar(a, b) {
+  return a + b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multi(a, b) {
+  return a * b;
+}
+
+module.exports = somar;
